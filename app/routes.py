3c65118cc4d807b4f60152ecb38c5cec9830bbc8
strunk23 <miloszwiktor58>
2023-06-22 17:52:51 +0200

    Improved document selection

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
 import os
+from os import listdir
+from os.path import isfile, join
 from app import app
 from scraper import scraper
 from flask import render_template, request, redirect, url_for, json
@@ -13,12 +15,15 @@ def run_scraper():
     if request.method == 'POST':
         global code
         code = request.form['code']
-        scraper.scraper(str(code))
+        onlyfiles = [f for f in listdir('opinions') if isfile(join('opinions', f))]
+        all_codes = [f[:-5] for f in onlyfiles]
+        if code not in all_codes:
+            scraper.scraper(code)
         return redirect(url_for('results'))
     else:
         return render_template('index.html.jinja')
     
 @app.route('/results', methods=['GET', 'POST'])
 def results():
-    return render_template('results.html.jinja', jsonfile=json.dumps(json.load(open(f'scraper/opinions/{code}.json', encoding='utf-8'))))
+    return render_template('results.html.jinja', jsonfile=json.dumps(json.load(open(f'opinions/{code}.json', encoding='utf-8'))))
     
